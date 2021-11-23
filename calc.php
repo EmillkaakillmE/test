9aa6a519c102a2081eee2394824d35b7b54c3386
Emil <68506597+EmillkaakillmE@users.noreply.github.com>
2021-11-23 16:26:08 +0300

    Давай

--- calc.php
+++ calc.php
@@ -1,26 +1,5 @@
 <?php
-function dump($arr, $var = false, $die = false)
-{
-    $bt = debug_backtrace();
-    $bt = $bt[0];
-    $dRoot = $_SERVER["DOCUMENT_ROOT"];
-    $dRoot = str_replace("/", "\\", $dRoot);
-    $bt["file"] = str_replace($dRoot, "", $bt["file"]);
-    $dRoot = str_replace("\\", "/", $dRoot);
-    $bt["file"] = str_replace($dRoot, "", $bt["file"]);
-    ?>
-	<div style='font-size:9pt; color:#000; background:#fff; border:1px dashed #000;'>
-		<div style='padding:3px 5px; background:#99CCFF; font-weight:bold;'>File: <?= $bt["file"] ?> [<?= $bt["line"] ?>
-			]
-		</div>
-		<pre style='padding:10px;'><? $var ? var_dump($arr) : print_r($arr); ?></pre>
-	</div>
-    <?
-    if($die) {
-        die;
-    }
-}
-
+require_once 'assets/back_functions/dump.php';
 $post = [];
 //Безопасим
 foreach($_POST as $key => $value) {
@@ -30,37 +9,54 @@ $result = [];
 
 //Валидация на бэке. Основная валидация была на фронте
 foreach($post as $name => $value) {
-    if(empty($value) || $value == false) {
+    if($value === false && $name !== 'fill-per-month') {
         $result['STATUS'] = 'ERROR';
         $result['CODE'] = 500;
-        echo json_encode($post);
+        echo json_encode($name);
         exit(0);
     }
 }
 
 //region Полезные переменные
-$prevMonthSum = intval($post['sum']);
-$sumAdd = intval($post['sumAdd']);
-$percent = intval($post['percent']);
+$prevMonthSum = intval(str_replace(' ', '', $post['sum']));
+$sumAdd = intval(str_replace(' ', '', $post['sumAdd']));
+$percent = intval($post['percent']) / 100;
 
 $dateStart = DateTime::createFromFormat('d.m.Y', $post['startDate']);
 $dateStart->setTimezone(new DateTimeZone('Europe/Moscow'));
 $daysY = $dateStart->format('L') ? 366 : 365;
 
 $dateEnd = clone $dateStart;
-$dateEnd->modify("+$post[term] ".$post['year-month']);
+$dateEnd->modify("+$post[term] " . $post['year-month']);
 
 $interval = $dateStart->diff($dateEnd);
 $fullMonthDiff = (intval($interval->format('%y') * 12)) + intval($interval->format('%m'));
 
-$daysN = intval($dateStart->format('t'));
+//Количество дней оставшихся до конца нулевого месяца. (Тот месяц, в который был совершен вклад)
+$daysN = intval($dateStart->format('t')) - intval($dateStart->format('d'));
 //endregion
 
 
-$result['RESULT_1'] = round($prevMonthSum + ($prevMonthSum + $sumAdd) * $daysN * ($percent/$daysY));
+$monthN = clone $dateStart;
+$monthCounter = $fullMonthDiff;
+while($monthCounter > 0) {
+    //Для не нулевого месяца берём переменную daysN и $daysY за циклом. Для последующих месяцев расчитываем
+    if($monthCounter != $fullMonthDiff) {
+        $monthN->modify("+1 month");
+        $daysN = $monthN->format('t');
+        //На случай, если год будет высокосным в этом расчёте
+        $daysY = $monthN->format('L') ? 366 : 365;
+    }
+    $prevMonthSum += ($prevMonthSum + $sumAdd) * $daysN * ($percent / $daysY);
+    $monthCounter--;
+}
+
+$res = intval(round($prevMonthSum));
+$result['RESULT_1'] = $res;//round($prevMonthSum + ($prevMonthSum + $sumAdd) * $daysN * ($percent/$daysY));
 
 //https://www.raiffeisen.ru/wiki/kak-rasschitat-procenty-po-vkladu/ Ежемесячная капитализация
-$result['RESULT_2'] = round(($prevMonthSum + $sumAdd) * pow((1 + $percent/100), $interval->days));
+//$result['RESULT_2'] = round((intval(str_replace(' ', '', $post['sum'])) + intval(str_replace(' ', '', $post['sum']))) * pow((1 + $percent / 100), $fullMonthDiff));
 $result['CODE'] = 200;
 $result['STATUS'] = 'OK';
+
 echo json_encode($result);
